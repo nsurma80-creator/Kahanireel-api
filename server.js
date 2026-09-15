27991b53f03c35dedd18341e6a256931dc0fcf84
nsurma80-creator <nsurma80@gmail.com>
2026-09-15 12:38:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+const http = require("http");
 
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+
+  res.end(JSON.stringify({
+    success: true,
+    message: "KahaniReel API is working!"
+  }));
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`KahaniReel API running on port ${PORT}`);
+});
